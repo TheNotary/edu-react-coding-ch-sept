a426259f8b44113a8ace6d154d663697718fe028
TheNotary <no@email.plz>
2020-09-11 12:20:23 -0500

    basic scaffolding compelte

---
+++ src/features/sectionA/SectionA.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SectionA = () => {
+  return (
+    <>
+      <h1>Section A</h1>
+      <div>I'm Section A!</div>
+    </>
+  );
+};
+
+export default SectionA;
---
+++ src/features/static/HomePage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "../../logo.svg";
+
+const HomePage = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Home Page</h1>
+      </header>
+    </div>
+  );
+};
+
+export default HomePage;
